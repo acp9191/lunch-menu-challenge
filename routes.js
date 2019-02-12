47f5ae372aa8b926fe27ef6f561a10918d93fcdb
Avery Peterson <peterson.a@husky.neu.edu>
2019-02-12 12:06:45 -0500

    Cleaning up code and adding descriptive comments

--- routes.js
+++ routes.js
@@ -13,20 +13,24 @@ var appRouter = function (app) {
 
     rp(options)
     .then(($) => {
-      // assume first slide in Carousel is today
+      // assume first slide (0) in Carousel is today
       let todaysMenu = $('.foodCarousel--day').eq(0);
       let todaysDate = todaysMenu.find('.dayheadline').text();
       let menuItems = [];
+
       todaysMenu.find('li').each(function() {
         let item = $(this);
         menuItems.push({
           title: item.find('.product--title .cobie-product-name').text(),
-          allergens: item.find('.product--title sup').text().split(", ").map((x) => parseInt(x)),
+          // split and map to integer
+          allergenIds: item.find('.product--title sup').text().split(", ").map((x) => parseInt(x)),
+          // use Regex to get numbers from string, then parse as float
           price: parseFloat(item.find('.woocommerce-Price-amount').text().replace(/[^0-9.-]+/g,"")),
-          currency: (item.find('.woocommerce-Price-currencySymbol').text()) == '€' ? "EUR" : "other",
+          // convert currency symbol to 3 letter abbreviation 
+          currency: (item.find('.woocommerce-Price-currencySymbol').text()) === '€' ? "EUR" : "other",
           description: item.find('.cobie-product-description').text()
-        })
-      })
+        });
+      });
       res.status(200).send({ 
         data: {
           todaysDate: todaysDate,
